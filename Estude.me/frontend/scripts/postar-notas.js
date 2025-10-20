8ce1a2d97bb3d62b2be793384007b630421b6986
Letícia Lisboa <133930947+lelisboa07@users.noreply.github.com>
2025-10-20 02:39:20 -0300

    Delete Estude.me directory

--- Estude.me/frontend/scripts/postar-notas.js
+++
@@ -1,7 +0,0 @@
-const botoaoSalvar = document.querySelectorAll('.button-salvar')
-
-botoaoSalvar.forEach((botao) => {
-  botao.addEventListener('click', () => {
-    window.location.href = '/frontend/professor/notas.html'
-  })
-})
